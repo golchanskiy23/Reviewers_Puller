5cc74463f7c622171683fc4c4fd1ade2b855d88a
Maxim <golchanskiy23@gmail.com>
2025-11-20 19:00:00 +0300

    Skeleton of all execution services and repositories

--- internal/app/run.go
+++ internal/app/run.go
@@ -1,7 +1,45 @@
 package app
 
-import "Service-for-assigning-reviewers-for-Pull-Requests/config"
+import (
+	"Service-for-assigning-reviewers-for-Pull-Requests/config"
+	"Service-for-assigning-reviewers-for-Pull-Requests/pkg/database/postgres"
+	"fmt"
+)
+
+/*
+func InitDB(db *postgres.DatabaseSource, path string) error {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("error reading file: %v", err)
+	}
+	if _, err = db.Pool.Exec(context.Background(), string(file)); err != nil {
+		return fmt.Errorf("error executing sql: %v", err)
+	}
+	givenOrder, err := utils.GetGivenOrder()
+	if err != nil {
+		return fmt.Errorf("error getting givenOrder: %v", err)
+	}
+
+	if err = postgres.AddOrdersToDB(db, givenOrder); err != nil {
+		return fmt.Errorf("error adding orders to database: %v", err)
+	}
+	return nil
+}*/
 
 func Run(cfg *config.Config) error {
+	db, err := initPostgres(cfg)
+	if err != nil {
+		return err
+	}
+	defer func(db *postgres.DatabaseSource) {
+		db.Close()
+	}(db)
+	
+	pgRepository := initDBRepository(db)
+	service := CreateNewOrderService(pgRepository)
+	orderController := controller.CreateNewOrderController(orderService)
+	if err = startServer(cfg, orderController); err != nil {
+		return fmt.Errorf("start server error: %v", err)
+	}
 	return nil
 }
